b2e0ce9a45e61f9e61a5f2286c4e1d8967a99b1f
Craig McKeachie <craigdmckeachie@gmail.com>
2023-05-08 15:05:08 -0400

    array example

--- examples/2-12_arrays.js
+++ examples/2-12_arrays.js
@@ -1,10 +1,17 @@
 "use strict";
 
 let kids = ["Natalie", "Brittany", "Zachary"];
-let firstKid = kids[0];
-console.log(firstKid);
-console.log(typeof firstKid);
-console.log(kids[0]);
-console.log(typeof kids[0]);
-console.log(kids[1]);
-console.log(kids[2]);
+
+for (let i = 0; i < kids.length; i++) {
+  const element = kids[i];
+  console.log(element);
+  console.log(typeof element);
+}
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let i = 0; i < numbers.length; i++) {
+  const element = numbers[i];
+  console.log(element);
+  console.log(typeof element);
+}
