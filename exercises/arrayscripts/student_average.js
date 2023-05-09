8937118569eec1e3259fe04840f7e1e5f085ee42
Craig McKeachie <craigdmckeachie@gmail.com>
2023-05-09 13:20:20 -0400

    added comments

--- exercises/arrayscripts/student_average.js
+++ exercises/arrayscripts/student_average.js
@@ -1,16 +1,16 @@
 let students = [
-  { name: "Zephaniah", scores: [100, 96, 99, 92] },
+  { name: "Zephaniah", scores: [100, 96, 99, 92] }, //an array of test scores for a particular student
   { name: "Pursalane", scores: [92, 89, 96, 100, 94] },
   { name: "Siddalee", scores: [86, 72, 92] },
   { name: "Ian", scores: [98, 84, 89, 100, 100, 76] },
   { name: "Elisha", scores: [89, 100] },
   { name: "Ezra", scores: [100, 99, 100, 87] },
-];
+]; //array of objects representing students
 
 for (let index = 0; index < students.length; index++) {
   const student = students[index];
   console.log(student.name);
-  
+
   let totalOfAllScores = 0;
   for (let index = 0; index < student.scores.length; index++) {
     const score = student.scores[index];
@@ -20,8 +20,6 @@ for (let index = 0; index < students.length; index++) {
   let numberOfScores = student.scores.length;
   let averageScore = totalOfAllScores / numberOfScores;
   console.log(averageScore);
-
-
 }
 
 // let student2 = students[1];
