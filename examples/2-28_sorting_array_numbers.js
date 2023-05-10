f9fba2a345a99b25a8b3e4ba4f9500cb8dd43227
Craig McKeachie <craigdmckeachie@gmail.com>
2023-05-10 14:24:38 -0400

    sort array numbers

--- examples/2-28_sorting_array_numbers.js
+++ examples/2-28_sorting_array_numbers.js
@@ -1,3 +1,18 @@
+function compareAscendingNumber(a, b) {
+  if (a < b) return -1; // right order
+  else if (a == b) return 0; // same values
+  else return 1; // swap, wrong order
+}
+
 let numbers = [3, 27, 400, 1, 111, 5];
-numbers.sort();
+numbers.sort(compareAscendingNumber);
+console.log(numbers);
+
+console.log("---------------");
+
+numbers.sort(function (a, b) {
+  if (a > b) return -1; // right order
+  else if (a == b) return 0; // same values
+  else return 1; // swap, wrong order
+});
 console.log(numbers);
