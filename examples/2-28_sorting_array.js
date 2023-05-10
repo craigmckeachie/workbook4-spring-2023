f107730b4d9a97086ffefad06872f6020e93d6b6
Craig McKeachie <craigdmckeachie@gmail.com>
2023-05-10 13:34:57 -0400

    sorting array

---
+++ examples/2-28_sorting_array.js
@@ -0,0 +1,9 @@
+let kids = ["Natalie", "Brittany", "Zachary"];
+console.log(kids);
+kids.sort(); // Sorts the array
+console.log(kids);
+
+let numKids = kids.length;
+for (let i = 0; i < numKids; i++) {
+  console.log(kids[i]);
+}
