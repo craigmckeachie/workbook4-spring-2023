9942cd93029afa1b3468863de782a37515767fed
Craig McKeachie <craigdmckeachie@gmail.com>
2023-05-10 22:02:49 -0400

    median numbers challenge

--- exercises/arrayscripts/median_of_numbers.js
+++ exercises/arrayscripts/median_of_numbers.js
@@ -1,18 +1,32 @@
-let numbers1 = [83, 80, 92, 86, 100];
+
 
 function numericSortAsc(a, b) {
   return a - b;
 }
 
-function hasEvenNumberOfElements(array) {
-  return array.length % 2 == 0;
+function hasOddNumberOfElements(array) {
+  return array.length % 2;
+}
+
+function getIndexOfMiddleElement(array) {
+  return Math.floor(array.length / 2);
 }
 
 function getMedian(numbers) {
-  let sorted = numbers.sort(numericSortAsc);
-  console.log(sorted);
-  let isEven = hasEvenNumberOfElements(sorted);
-  let half = Math.floor(sorted.length / 2);
+  numbers.sort(numericSortAsc);
+  let indexOfMiddleElement = getIndexOfMiddleElement(numbers);
+  let middleElement = numbers[indexOfMiddleElement];
+  let elementBeforeMiddleElement = numbers[indexOfMiddleElement - 1];
+
+  if (hasOddNumberOfElements(numbers)) {
+    return middleElement;
+  } else {
+    return (elementBeforeMiddleElement + middleElement) / 2;
+  }
 }
 
-getMedian(numbers1);
+let numbers1 = [83, 80, 92, 86, 100];
+let numbers2 = [80, 83, 86, 87, 92, 100];
+
+console.log(getMedian(numbers1));
+console.log(getMedian(numbers2));
